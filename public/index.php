f98b24b9f9992444868a5282c3bf58053d3bef11
Bill Hicks <billvanhausen@yahoo.com>
2025-09-30 15:44:44 -0400

    Slim and Twig implementation

--- public/index.php
+++ public/index.php
@@ -1,15 +1,80 @@
 <?php
+/*
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Twig;
+use Slim\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
+$twig = Twig::create("../templates", ['cache' => false]);
+
+$app->add(TwigMiddleware::create($app, $twig));
+
+$app->get('/', function (Request $request, Response $response) {
     $response->getBody()->write("Hello world!");
     return $response;
 });
 
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $response->getBody()->write("Hello " . ucfirst( $request->getAttribute('name') ) . "!");
+    return $response;
+});
+
+$app->run();
+*/
+
+use DI\Container;
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Create Container
+$container = new Container();
+
+// Set view in Container
+$container->set(Twig::class, function() {
+    return Twig::create(__DIR__ . '/../templates', ['cache' => '/../cache']);
+});
+
+// Create App from container
+$app = AppFactory::createFromContainer($container);
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
+
+// Add other middleware
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
+
+// Render from template file templates/profile.html.twig
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    $viewData = [
+        'name' => ucfirst( $args['name'] ),
+    ];
+
+    $twig = $this->get(Twig::class);
+
+    return $twig->render($response, 'profile.html.twig', $viewData);
+})->setName('profile');
+
+// Render from string
+$app->get('/hi/{name}', function ($request, $response, $args) {
+    $viewData = [
+        'name' => $args['name'],
+    ];
+
+    $twig = $this->get(Twig::class);
+    $str = $twig->fetchFromString('<p>Hi, my name is {{ name }}.</p>', $viewData);
+    $response->getBody()->write($str);
+
+    return $response;
+});
+
+// Run app
 $app->run();
